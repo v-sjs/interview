c2d2b7850333c580fa09a82f08bee432584502b9
vipsjs <249128843@qq.com>
2021-03-09 14:42:41 +0800

    11

--- test.js
+++ test.js
@@ -52,13 +52,83 @@
 // }, []);
 // console.log(flatten(list));
 
-Function.prototype.myApply = function (context, args) {
-    context.fn = this;
-    let res;
-    if (!args) {
-        res = context.fn();
-    } else {
-        res = context.fn(...args);
-    }
-    return res;
-};
+// Function.prototype.myApply = function (context, args) {
+//     context.fn = this;
+//     let res;
+//     if (!args) {
+//         res = context.fn();
+//     } else {
+//         res = context.fn(...args);
+//     }
+//     return res;
+// };
+
+// console.log(1);
+// setTimeout(() => {
+//     console.log(2);
+//     Promise.resolve().then(data => {
+//         console.log(3);
+//     })
+// })
+// new Promise((resolve) => {
+//     resolve()
+//     console.log(4);
+// }).then(() => {
+//     console.log(5);
+//     setTimeout(() => {
+//         console.log(6);
+//     })
+// }).then(() => { console.log(7); })
+// console.log(8);
+// // 14857236
+// var nickname = 'lilei'
+// function Person(name) {
+//     this.nickname = name;
+//     this.sayHi = function () {
+//         console.log(this.nickname + 'Ps');
+//         setTimeout(function () {
+//             console.log(this.nickname);
+//         }, 1000)
+//     }
+// }
+// var Male = {
+//     nickname: 'xiaofang',
+//     sayHi: () => {
+//         console.log(this.nickname);
+//     }
+// }
+
+// var person = new (Person.bind(Male, 'xiaohong'))
+// console.log(Male);
+// person.sayHi()
+// function fn1() {
+//     console.log(this, arguments);
+// }
+// function fn2() {
+//     console.log(2);
+// }
+// fn1.call.call(fn2)
+
+// function Animal(type) {
+//     this.type = type;
+// }
+// Animal.prototype.say = function (word) {
+//     console.log(word);
+// }
+// mkNew = function () {
+//     let constructor = [].shift.call(arguments)
+//     let obj = {}
+//     obj.__proto__ = constructor.prototype //创建的对象指向原型上的方法
+//     let r = constructor.apply(obj, arguments)
+//     return r instanceof Object ? r : obj
+// }
+// let animal = mkNew(Animal, '哺乳🥱')
+
+// console.log(animal.type);
+// animal.say('nidaye')
+compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
+const greeting = (name) => `hello ${name}`;
+const toUpper = (str) => str.toUpperCase();
+const fn = compose(toUpper, greeting);
+console.log(fn("sunny"));
+// HELLO SUNNY
